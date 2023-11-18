13ba6c2da9def87ee63ea3f166927a3fa6b0e950
Emily Perea Córdoba <emilycodesoft@gmail.com>
2023-11-18 16:10:53 -0500

    Delete ungroup data link from the website

--- src/views/DescriptiveIndexView.vue
+++ src/views/DescriptiveIndexView.vue
@@ -2,9 +2,6 @@
   <div class="d-flex flex-column align-items-center">
     <h2>Escoge el tipo de dato</h2>
     <div class="d-flex flex-column align-items-center">
-      <router-link :to="{ name: 'UngroupData' }">
-        <button class="btn btn-primary mb-4">Datos no tabulados</button>
-      </router-link>
       <router-link :to="{ name: 'GroupData' }">
         <button type="button" class="btn btn-primary">Datos tabulados</button></router-link
       >
